80ad8fa861f1a12309af2e8698233ae1c2cb26dc
KerttuRungi <kerttu.rungi@gmail.com>
2025-05-14 16:08:08 +0300

    Add source code

--- Advanced AJAX/obj/Debug/net8.0/Advanced AJAX.AssemblyInfo.cs
+++ Advanced AJAX/obj/Debug/net8.0/Advanced AJAX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advanced AJAX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0eec99b0b6083513063b0ff98f80f392e09b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advanced AJAX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advanced AJAX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
