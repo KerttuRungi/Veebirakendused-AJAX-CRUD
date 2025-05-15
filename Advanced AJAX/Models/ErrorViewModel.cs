2d7af8cee01833b6208049dfcf45c15dd1e17b46
KerttuRungi <kerttu.rungi@gmail.com>
2025-05-15 19:17:39 +0300

    Add Country and City

--- Advanced AJAX/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Advanced_AJAX.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
